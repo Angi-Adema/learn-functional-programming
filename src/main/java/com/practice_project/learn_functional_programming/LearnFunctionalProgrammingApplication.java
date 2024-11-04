55dd678bd832408b06bdbf190bc7651db95fa7c2
Angi-Adema <dr.adema@gmail.com>
2024-11-04 12:22:50 -0700

    save new project intro files created by Spring

---
+++ src/main/java/com/practice_project/learn_functional_programming/LearnFunctionalProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.practice_project.learn_functional_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFunctionalProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFunctionalProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice_project/learn_functional_programming/LearnFunctionalProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice_project.learn_functional_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnFunctionalProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
