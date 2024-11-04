e51597ac5f37cd28ee41aa9e5e60e588a19b1d1b
Angi-Adema <dr.adema@gmail.com>
2024-11-04 16:05:14 -0700

    added logic in Challenge1Structured.java to print the even numbers in a stream, added logic in Challenge1Functional.java to utilize functional programming and lambda expressions to print the even numbers in a stream.

--- src/main/java/com/practice_project/learn_functional_programming/programming/Challenge1Functional.java
+++ src/main/java/com/practice_project/learn_functional_programming/programming/Challenge1Functional.java
@@ -6,20 +6,29 @@ public class Challenge1Functional {
 
 	public static void main(String[] args) {
 		
-		printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+		
+		//printAllNumbersInListFunctional(numbers);
+		printEvenNumbersInListFunctional(numbers);
 
 	}
 	
-	private static void print(int number) {
-		System.out.println(number);
-	}
+//	private static void print(int number) {
+//		System.out.println(number);
+//	}
+	
+	// Check if the number in number is even or not. Now that we have a lambda, we do not need this method any more.
+//	private static boolean isEven(int number) {
+//		return number % 2 == 0;
+//	}
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
 		
 		// In a functional approach, we focus on what to do. We want to convert the list of numbers [12,9,13,4,6,2,4,12,15] into a stream of numbers or sequence of numbers on their own line.
 		// Instead of getting a list like this: [12,9,13,4,6,2,4,12,15], we get a stream where the numbers are printed sequentially on a separate line.
 		numbers.stream()
-			.forEach(Challenge1Functional::print);  // Name of the class followed by the name of the method. For each element, the Challenge1Functional class is called and the print method is executed.
+			//.forEach(Challenge1Functional::print);  // Name of the class followed by the name of the method. For each element, the Challenge1Functional::print method is called.
+		      .forEach(System.out::println);  // System.out contains a static method called println. We can dynamically call this and we no longer need the print method above.
 		
 		// How do we loop all the numbers?
 //		for(int number : numbers) {
@@ -27,5 +36,19 @@ public class Challenge1Functional {
 //		}
 		
 	}
+	
+	// How do we print the even numbers in a functional approach?
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+		
+		numbers.stream()
+			  // To print only the even numbers, we add a Filter to only print the even numbers. We want to call isEven on all the numbers and if it is even then proceed to the action of printing.
+			 // .filter(Challenge1Functional::isEven)
+			  .filter(number -> number % 2 == 0)   // We define a lambda expression rather than just the filter and method reference. Take number as the parameter and check if number % 2 == 0 and return it back.
+		      .forEach(System.out::println);  
+		
+	}
 
 }
+
+// In the structural approach, we have to focus on how to move the numbers and then print them. Here in the functional approach, we get a list of elements and then define what we want to do with each of them.
+// We are saying that for each element in the stream, do a print for each one.
--- src/main/java/com/practice_project/learn_functional_programming/programming/Challenge1Structured.java
+++ src/main/java/com/practice_project/learn_functional_programming/programming/Challenge1Structured.java
@@ -5,17 +5,32 @@ import java.util.List;
 public class Challenge1Structured {
 
 	public static void main(String[] args) {
+
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 		
-		printAllNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+		//printAllNumbersInListStructured(numbers);
+		printEvenNumbersInListStructured(numbers);
 
 	}
 
 	private static void printAllNumbersInListStructured(List<Integer> numbers) {
 		// How do we loop all the numbers?
-		for(int number : numbers) {
+		for (int number : numbers) {
 			System.out.println(number);
 		}
-		
+
+	}
+
+	// What if we only want to print all the even numbers in the list?
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+		// How do we loop all the numbers?
+		for (int number : numbers) {
+			// if(even), print the number out.
+			if (number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+
 	}
 
 }
